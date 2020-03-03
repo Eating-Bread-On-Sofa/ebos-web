58791ff07e9ecfe0caf109e96a9684cfa2949f62
and_lzn <16721059@bjtu.edu.cn>
2020-03-03 09:57:36 +0800

    评论

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Rule from '@/components/Rule'
+import Login from '../components/Login'
+import Register from '../components/Register'
 
 Vue.use(Router)
 
@@ -16,6 +18,20 @@ export default new Router({
       path: '/Rule',
       name: 'Rule',
       component: Rule
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/',
+      redirect:'/login'
     }
   ]
 })
